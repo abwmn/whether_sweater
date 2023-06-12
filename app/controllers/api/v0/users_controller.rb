29ad994446b93a71a45e41f8a66f9417eb3c97c3
Andrew Bowman <andrewtbowman@gmail.com>
2023-06-11 23:25:34 -0400

    feat: user registration

---
+++ app/controllers/api/v0/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V0::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: { error: user.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       get 'forecast', to: 'forecasts#show'
+      resources :users, only: [:create]
     end
   end
 end
