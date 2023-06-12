b8c755d7a698e167879414d3998ad888ae138682
Andrew Bowman <andrewtbowman@gmail.com>
2023-06-12 12:02:55 -0400

    test: check every key and attribute of response

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,4 +1,4 @@
-class Api::V0::BooksController < ApplicationController
+class Api::V1::BooksController < ApplicationController
   def search(params)
     forecast = ForecastRetriever.new(params[:location]).call
     qty = params[:qty]
