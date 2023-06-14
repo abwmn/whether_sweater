7743172e32b1d2154960c7da39e4d76f4d6b7445
Andrew Bowman <andrewtbowman@gmail.com>
2023-06-14 02:04:00 -0400

    refactor service into facade

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,18 @@
+class ForecastFacade
+  def self.get_forecast(location, time = Time.now)
+    coordinates = MapService.get_coordinates(location)
+    forecast = WeatherService.get_forecast(coordinates, time)
+    Forecast.new(forecast)
+  end
+
+  def self.condensed_forecast(location)
+    forecast = get_forecast(location).data
+    {
+      'destination' => location,
+      'forecast' => {
+        'summary' => forecast[:current][:condition][:text],
+        'temperature' => "#{forecast[:current][:temp_f]} F"
+      }
+    }
+  end
+end
