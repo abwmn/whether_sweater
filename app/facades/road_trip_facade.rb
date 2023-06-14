0bfe1e2e3ab3f52f69f60efaa2ee11d85629d1ef
Andrew Bowman <andrewtbowman@gmail.com>
2023-06-14 04:18:07 -0400

    refactor: break up road trip facade methods more

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -4,28 +4,29 @@ class RoadTripFacade
     return RoadTrip.new(params[:origin], params[:destination], "impossible", {}) unless travel_times
 
     forecast = ForecastFacade.get_forecast(params[:destination])
-    eta_forecast = get_eta_forecast(forecast, travel_times.first)
+    eta_forecast = calculate_eta_forecast(forecast, travel_times.first)
 
     RoadTrip.new(params[:origin], params[:destination], travel_times.last, eta_forecast)
   end
 
   private
 
-  def self.get_eta_forecast(forecast, travel_time_seconds)
+  def self.calculate_eta_forecast(forecast, travel_time_seconds)
     local_time = DateTime.parse(forecast.data[:location][:localtime])
     eta = local_time.advance(seconds: travel_time_seconds)
-    extract_eta_forecast(forecast, eta)
+    hourly_forecast = extract_hourly_forecast(forecast, eta)
+    define_eta_forecast(hourly_forecast)
   end
 
-  def self.extract_eta_forecast(forecast, eta)
+  def self.extract_hourly_forecast(forecast, eta)
     eta_date = eta.to_date.to_s
-
     forecast_day = forecast.data[:forecast][:forecastday].find do |day|
       day[:date] == eta_date
     end
-
-    hourly_forecast = forecast_day[:hour][eta.hour]
-
+    forecast_day[:hour][eta.hour]
+  end
+  
+  def self.define_eta_forecast(hourly_forecast)
     {
       datetime: hourly_forecast[:time],
       temperature: hourly_forecast[:temp_f],
